7ad12ba22c2975868a6c12c5975c223cb4d5dc1b
George Muratov <sedoy@Georges-MacBook-Air.local>
2019-02-04 12:56:23 +0100

    db host env var changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 3000
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-const dbHost = process.env.MONGO_DB_HOST || 'localhost:27017'
+const dbHost = process.env.MONGODB_URI || 'localhost:27017'
 const dbName = process.env.DB_NAME || 'strvDB'
 
 const config = {
